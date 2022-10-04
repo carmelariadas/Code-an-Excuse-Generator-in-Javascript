34549eb92f71977f7134563de7472a175240fc2e
carmelariadas <carmen.mk.167@gmail.com>
2022-10-04 17:41:40 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  console.log(who[randomWho] + what[randomWhat] + when[randomWhen]);
+
+  document.querySelector("#excuse").innerHTML =
+    who[randomWho] + what[randomWhat] + when[randomWhen];
 };
